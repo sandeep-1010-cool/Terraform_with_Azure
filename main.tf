b0d64daf7cb85edae1cf167092189e94ed7e56e8
Vuyyala sandeep kumar <vuyyalasandeep1010@gmail.com>
2025-07-16 14:16:20 +0530

    Remove variables.tf file as it is no longer needed for the project.

---
+++ main.tf
@@ -0,0 +1,19 @@
+# main.tf
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    # Backend configuration will be provided via -backend-config flag
+  }
+}
+
+provider "azurerm" {
+  features {}
+} 
